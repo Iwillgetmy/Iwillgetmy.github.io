55c429e029b9746ad3fb15e008dd02ed9592af63
Iwillgetmy <dkanesal3@gmail.com>
2022-03-14 11:11:42 -0400

    updates

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,5 @@
 function toggleMenu() {
-    const menuToggle = document.querySelector('.toggle');
-    const sidebar = document.querySelector('.sidebar');
-    const text = document.querySelector('.text');
-    const name = document.querySelector('.name');
-    menuToggle.classList.toggle('active');
-    sidebar.classList.toggle('active');
-    text.classList.toggle('active');
-    name.classList.toggle('active');
+    const navToggle = document.querySelector('.navbar');
+    navToggle.classList.toggle('active');
 }
+
